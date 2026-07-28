35b370cf110d2cbc1f3d3502ceca850afd7f2076
iamsivaone <iamsivaone@gmail.com>
2026-07-28 22:42:22 +0530

    Add env example and ignore secrets

--- app/services/llm.py
+++ app/services/llm.py
@@ -6,11 +6,12 @@ from langchain.chat_models import init_chat_model
 
 load_dotenv()
 
+# currently using groq for testing, but can switch to google if needed
 google_llm = ChatGoogleGenerativeAI(
     model="gemini-3.6-flash", google_api_key=os.getenv("GOOGLE_API_KEY"), temperature=0
 )
 
-
+# currently using this groq model for testing, but can switch to google if needed
 llm = init_chat_model(
             model="openai/gpt-oss-20b",
             model_provider="groq",
